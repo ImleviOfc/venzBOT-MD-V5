eaf05d08004f40930e75efff15954fcf99feb9ea
levi <111189476+ImleviOfc@users.noreply.github.com>
2022-08-14 16:42:54 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -11,7 +11,7 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 ------- ${nameown} -------
 
 📮 *Note:*
-• Owner tidak menerima save contact
+• Owner menerima save contact
 • Owner berhak blockir tanpa alasan
 • Berbicaralah yang sopan & tidak spam
 • Owner Hanya merespon yang berkaitan dengan BOT
@@ -21,13 +21,13 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-*💌 Nama* : Fangz
-*✉️ Nama RL* : FangzBOT
+*💌 Nama* : levi
+*✉️ Nama RL* : venz-BOT
 *♂️ Gender* : Laki - laki
 *🕋 Agama* : Islam
-*⏰ Tanggal lahir* : 25 oktober 2007
-*🎨 Umur* : 15
-*🧮 Kelas* : 9
+*⏰ Tanggal lahir* : 7 desember 2007
+*🎨 Umur* : 14
+*🧮 Kelas* : 7
 *🧩 Hobby* : Nonton henta*, main game, Recode script
 *💬 Sifat* : Baik, hode, softboy, tydack ramah, g*y
 *🗺️ Tinggal* : Indonesia, jawa, bogor
@@ -49,8 +49,8 @@ let teksbio = `${htki} *BIODATA* ${htka}
 🥏 *Whatsapp* wa.me/${nomorown}
 🌐 *Discord:* Soon
 🌏 *Website:* https://fangzbot.websites.co.in/
-🎶 *Tiktok:* tiktok.com/@fangzjb
-
+🎶 *Tiktok:* tiktok.com/@leviatnansyah
+t
 `
   let teks = ' '
 const sections = [
